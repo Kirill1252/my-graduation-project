a99d052330fed795bd14b67f34100ac4f4ae4aed
lKlliri <kcernysenko252@gmail.com>
2022-06-30 19:14:19 +0300

    IronikaSite

--- website/settings.py
+++ website/settings.py
@@ -88,17 +88,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-if platform.system() == 'Linux':
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangodb',
-        'USER': 'dbuser',
-        'PASSWORD': 'django',
         'HOST': 'localhost',
         'PORT': ''
     }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
